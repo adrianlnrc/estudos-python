689bdcecdd0dcbd5b194f0bfb50d040d0c1cdafa
Adrian Stelo <adrian.stelo@tarea.com.br>
2026-03-12 14:38:49 -0300

    feat: função consultar saldo

--- 02-pratica/caixa.py
+++ 02-pratica/caixa.py
@@ -1,35 +1,45 @@
 
 contas = {
-    "admin": 100.0,
-    "teste": 500.0,
+    'admin': 1000.00,
+    'visitante': 0.00
 }
 
-
 def consultar_saldo(usuario):
-    saldo = contas["admin"]
+    saldo = contas[usuario]
+    print(f"seu saldo atual é {saldo}!")
+   
+    pass
+def depositar(usuario, valor):
 
-print(consultar_saldo("admin"))
+
     
-def depositar(usuario):
-    ...
+    pass
+def sacar(usuario, valor):
+   pass
 
-usuario_atual = "admin"
+
+usuario_atual = 'admin'
+
 while True:
-    print(f"Bradesco: Bem vindo usuario: {usuario_atual} ")
-    print("(1)consultar saldo (2) depositar")
-
+    print(f"\n Caixa  Usuário: {usuario_atual}")
+    print("(1) Consultar | (2) Depositar | (3) Sacar | (4) Sair")
+    
     try:
-        opcao = int(input("Opção:"))
-
+        opcao = int(input("Opção: "))
+        
         if opcao == 1:
             consultar_saldo(usuario_atual)
-        
+            
         elif opcao == 2:
-            valor = int(input("Digite o valor a ser depositado"))
-            depositar(usuario_atual,valor)
-
-
-
-    except ValueError:
-        print("Erro")
-        
+            valor = float(input("Valor do depósito: "))
+            depositar(usuario_atual, valor)
+            
+        elif opcao == 3:
+            valor = float(input("Valor do saque:  "))
+            sacar(usuario_atual, valor)
+            
+        elif opcao == 4:
+            break
+            
+    except ValueError: 
+        print("\nERRO!")
