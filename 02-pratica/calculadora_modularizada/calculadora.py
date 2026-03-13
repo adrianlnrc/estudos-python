1852a662a951d89c8da93cd77d7ce4613e6dcfd2
Adrianlnrc <spottyadrian@gmail.com>
2026-03-13 17:29:56 -0300

    ADD: calculadora modularizada e jogo dragao

---
+++ 02-pratica/calculadora_modularizada/calculadora.py
@@ -0,0 +1,28 @@
+import operacoes
+
+print("""
+        --------------------
+            Calculadora
+        --------------------
+""")
+
+while True: 
+
+    try:
+        op = int(input("digite a operacao desejada: (1)soma (2)subtracao (3)multiplicacao (4)divisao (5) sair"))
+    
+        if op == 1:
+            resultado = operacoes.soma(0,0)
+        elif op == 2:
+            resultado = operacoes.subtracao(0,0)
+        elif op == 3: 
+            resultado = operacoes.multiplicacao(0,0)
+        elif op == 4:
+            resultado = operacoes.divisao(0,0)
+        else:
+            break
+
+        print(resultado)
+
+    except ValueError:
+        print("digite um numero válido")
