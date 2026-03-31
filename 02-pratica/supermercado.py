90485e43f1d114bea802e6b8d1557e94ecc3099b
Adrianlnrc <spottyadrian@gmail.com>
2026-03-31 16:00:35 -0300

    feat: função exibir produtos implementada, v2 supermercado

--- 02-pratica/supermercado.py
+++ 02-pratica/supermercado.py
@@ -1,42 +1,47 @@
 supermercado = {
-    "limpeza":{"detergente": 2.0,"amaciante": 20,"sabao em po": 16.90},
-    "Acouque":{"alcatra:": 45.90, "patinho moido": 42.90,"peito de frango": 20.00},
-    "padaria":{"pao": 14.90, "rosca": 5.00, "pizza": 29.90},
+    "limpeza":{"Detergente": 2.0,"Amaciante": 20,"Sabao em po": 16.90},
+    "acougue":{"Alcatra": 45.90, "Patinho moido": 42.90,"Peito de frango": 20.00},
+    "padaria":{"Pão": 14.90, "Rosca": 5.00, "Pizza": 29.90},
 
 }
 
-def ver_secoes():
-    print("\nSeções disponíveis")
+
+def exibir_secoes():
     for s in supermercado:
         print(s)
 
-def ver_produtos(secao):
-    print(supermercado[secao].items())
-
-
-
+def exibir_produtos(secao):
+    print(f"os produtos da secao: {secao}")
+    for produto, valor in supermercado[secao].items():
+        print(f'{produto} Valor R$ {valor}')
+    
 while True:
-    print("=" * 20)
+    print("=" * 35)
     print("Bem vindo ao supermercado girassol")
-    print("=" * 20)
+    print("=" * 35)
+    print("(1) Ver todas as secoes")
+    print("(2) Ver produtos de uma seção")
+    print("(3) Sair")
 
     try:
-        op = int(input("digite a opcao: (1) ver secoes (2) ver produtos (3) Sair -- "))
+        op = int(input("digite a opcao: "))
         
         if op == 1:
-            ver_secoes()
+            exibir_secoes()
         
         elif op == 2:
-            secao = input("de qual secao deseja ver os produtos? ")
-            ver_produtos(secao)
+            exibir_secoes()
+            secao = input("de qual secao deseja ver os produtos? ".lower())
+            exibir_produtos(secao)
+            if secao in supermercado:
+                exibir_produtos(secao)
+            else:
+                print("Secão nao encontrada!")
+        elif op == 3:
+            print("Obrigado, volte sempre!")
+            break 
         else:
-            print("Obrigado, volte sempre")
-            break
-
-            
-            
-
-
+            print("Opção inválida, Escolha 1,2 ou 3")
 
     except ValueError:
         print("Erro, digite apenas numeros na pagina principal")
