4d832565709692ba56347c4ce898b775c6f7c059
Adrian <spottyadrian@gmail.com>
2026-04-01 14:57:13 -0300

    add: water tracker v1, com função registrar quantidade ingerida e exibir total ingerido

--- 02-pratica/water-tracker.py
+++ 02-pratica/water-tracker.py
@@ -1,5 +1,61 @@
-consumo_agua = {'adrian': 0,'maria clara': 0}
+consumo_agua = {
+    "adrian": 0,
+    "maria clara": 0,
 
-def beber_agua(nome,ml):
-    consumo_agua[''] = ml
-    consumo_agua = {nome,ml}
+}
+
+meta_diaria = 2000
+
+def registrar_agua(nome,quantidade):
+    consumo_agua[nome] += quantidade 
+    total_consumido = consumo_agua[nome]
+    print(f" foi adicionado {quantidade} ml,  total {total_consumido}")
+
+def exibir_total():
+    print("Status da meta")
+
+    for nome, total_ml in consumo_agua.items():
+        if total_ml >= meta_diaria:
+            print(f"parabens, a meta {meta_diaria} foi batida: {total_ml}")
+        else: 
+            faltam = meta_diaria - total_ml
+            print(f"ainda falta pouco para bater a meta {meta_diaria}, total ingerido {total_ml} ")
+        print(f" {nome}: {total_ml}")
+    
+
+
+while True: 
+    print("=" * 40)
+    print("Contador de Agua")
+    print("=" * 40)
+    print("(1) Adicionar Agua ingerida")
+    print("(2) Exibir total de Agua ingerida")
+    print("(3) sair")
+
+    try:
+        op = int(input("Qual opcao:"))
+
+        if op == 1:
+            nome = input("De quem voce quer registrar: ".lower())
+            quantidade = int(input("quantos ml foram ingeridos? "))
+            registrar_agua(nome,quantidade)
+        
+        elif op == 2: 
+            exibir_total()
+        
+        else: 
+            print("valeu!")
+            break
+
+
+
+
+
+    except ValueError:
+        print("ERRO, selecione uma das opçoes")
+
+
+
+
+
+
