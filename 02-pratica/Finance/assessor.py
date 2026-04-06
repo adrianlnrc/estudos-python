faf6c516adc580291fd116896c112e3c9969499c
Adrian <spottyadrian@gmail.com>
2026-04-06 17:19:27 -0300

    Feat: Estrutura inicial Assessor.py

---
+++ 02-pratica/Finance/assessor.py
@@ -0,0 +1,38 @@
+import random
+
+# Gera um numero aleatorio
+numero_aleatorio = random.randint(0,10)
+
+def criar_pessoa():
+
+    """
+    oque precisa ser implementado: 
+    - Dicionario Vazio Pessoa = {}
+    - input com campos: nome, idade, perfil de investidor, Renda, Dividas, pretensao de ganhar mais
+    """
+    ...
+
+def investimentos():
+    """
+    exemplode de investimentos  que existem EX: renda fixa Pre e Pós Fixada, FIIs,  CDI, Variável (bitcoin, Ações)
+
+    """
+    
+    
+    ...
+
+
+
+
+
+while True:
+
+    try:
+        ...
+
+
+
+
+
+    except ValueError:
+        print("ERRO na página principal")
