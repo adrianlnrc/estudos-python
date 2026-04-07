a72cf9c0084307e7f8e9ba7709930e02c8f39ff8
Adrian <spottyadrian@gmail.com>
2026-04-07 11:22:37 -0300

    fix: lógica da funcao busca binaria

--- 02-pratica/06-advinhador.py
+++ 02-pratica/06-advinhador.py
@@ -1,21 +1,24 @@
 
-
 def binary_Search(arr, item):
     baixo = 0
     alto = len(arr) - 1
+    tentativas = 0
  
     while baixo <= alto: 
         meio = (baixo + alto) // 2
         chute = arr[meio]
-        usuario = input(f"numero:{chute} (Maior) (Menor) (Acertou)".lower())
-        if usuario == "maior":
+        usuario = input(f" O numero: {chute} é (Maior),(Menor) ou acertei? (Acertou): ").lower()
+        if usuario == "menor":
             baixo = meio + 1
-        elif usuario == "menor":
-            alto = meio -1
+            tentativas += 1
+        elif usuario == "maior":
+            alto = meio - 1
+            tentativas += 1
+        elif usuario == "acertou":
+            tentativas += 1
+            return (f"Em {tentativas} tentativas, Resultado: {chute}")
         else:
-            return (f" Acertei, seu numero: {num},  meu chute {chute}")
-        ...
-
+            print("Digite apenas: maior, menor ou acertou")
 
 while True:
     arr = list(range(1,101))
@@ -25,16 +28,10 @@ while True:
         if num < 1 or num > 100:
             print("digite apenas numeros entre 1 a 100")
         else:
-            print(num)
-            
-        resultado = binary_Search(arr,num)
-        print(resultado)
-        break
-
-
-    
-
-    
+            print(f"numero digitado: {num}")
+            resultado = binary_Search(arr,num)
+            print(resultado)
+            break
     except ValueError:
         print("ERRO, digite apenas numeros")
 
