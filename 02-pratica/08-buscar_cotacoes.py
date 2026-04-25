b8d7e012cadf0ab54add0e5907e77e785f4fb606
Adrianlnrc <spottyadrian@gmail.com>
2026-04-25 20:34:00 -0300

    init: busca de cotacoes com biblioteca requests

--- 02-pratica/08-buscar_cotacoes.py
+++ 02-pratica/08-buscar_cotacoes.py
@@ -1,9 +1,13 @@
 import requests 
 
-resposta = requests.get('https://github.com/adrianlnrc')
+resposta = requests.get('https://github.com/adrianlnrc') # transformando resposta em um objeto Response
+
+# Pegando todo tipo de retirada de informação do objeto 
+
+print("Codigo HTML da página:",resposta.text) #.text retorna o código HTML 
 
 print("Status da conexão",resposta.status_code) # verificar a conexão
 
-print("Codigo HTML da página:",resposta.text) #.text retorna o código HTML 
+
 
 
